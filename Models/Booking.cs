942c57f75a540a9ce560b7d91bfb618e3ee213ed
Adam Liljenberg <adam@cabanossy.se>
2023-02-27 17:35:35 +0100

    Updated booking class

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,14 +1,23 @@
+using System.Text.Json.Serialization;
 using Azure;
 using Azure.Data.Tables;
 
 public class Booking : ITableEntity
 {
-    public string Comment { get; set; }
-    public string Name { get; set; }
+    public string? Comment { get; set; }
+    public string? Name { get; set; }
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
-    public string PartitionKey { get; set; }
-    public string RowKey { get; set; }
+
+    //set partition key as year and month
+    [JsonIgnore]
+    public string PartitionKey
+    {
+        get => $"{Start.Year}{Start.Month:D2}";
+        set { }
+    }
+    public string RowKey { get; set; } = Guid.NewGuid().ToString();
     public DateTimeOffset? Timestamp { get; set; }
+    [JsonIgnore]
     public ETag ETag { get; set; }
 }
